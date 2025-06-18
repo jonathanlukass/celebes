5cff3dde95dfc679effa7de96cd7e114b9c2aff1
jonathanlukass <jhonatanlukas05@gmail.com>
2025-06-19 01:39:57 +0800

    style abouts

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
       <h1>Celebes <span>Essence </span></h1>
       <div className="about-content">
         {/* <img src="/About.jpeg" alt="Vegetables" /> */}
-        <p>Providing Great Foodstuffs<br />We have 30 years of agriculture & eco-farming experience globally, work with professionals</p>
+        <p>Pala Pilihan dari Sulawesi<br />Asli, Berkualitas, Tak Tertandingi!</p>
       </div>
     </section>
   );
