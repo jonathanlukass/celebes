9319a4de334c1cd0a829c47e74bbc524750c3339
AndreasTopuh <andreasjeno23@gmail.com>
2025-06-18 20:26:31 +0800

    adding first view

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className="about">
+      <h1>Agriculture <span>Vegetables</span></h1>
+      <div className="about-content">
+        <img src="/About.jpeg" alt="Vegetables" />
+        <p>Providing Great Foodstuffs<br />We have 30 years of agriculture & eco-farming experience globally, work with professionals</p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
