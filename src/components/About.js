2833a7aea111f458adc127bc29101868ed07a043
jonathanlukass <jhonatanlukas05@gmail.com>
2025-06-25 19:44:17 +0800

    fix

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,13 @@ import React from 'react';
 function About() {
   return (
     <section className="about">
+<<<<<<< Updated upstream
       <h1 className="about-title">
         Celebes<span> Essence </span>
       </h1>
+=======
+      <h1>Celebes <span>Essence </span></h1>
+>>>>>>> Stashed changes
       <div className="about-content">
         <p>Selected Nutmeg from North Sulawesi<br />Authentic, Premium, Unmatched!</p>
       </div>
